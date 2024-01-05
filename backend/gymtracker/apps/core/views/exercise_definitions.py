056d12c0b8947f8e68ed5a4d1c823be8bba38bc0
Martin <martinpazicky10@gmail.com>
2024-01-05 13:53:12 +0100

    cleaned ex_def

--- backend/gymtracker/apps/core/views/exercise_definitions.py
+++ backend/gymtracker/apps/core/views/exercise_definitions.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.db import connection, reset_queries
 from apps.core.utility import dictfetchall
-from django.db.models import  OuterRef, Subquery, F, IntegerField, FloatField
+from django.db.models import  OuterRef, Subquery, F, IntegerField, FloatField, Prefetch
 
 
 class ExerciseDefinitionsViewSet(viewsets.ModelViewSet):
@@ -46,36 +46,33 @@ class ExerciseDefinitionsViewSet(viewsets.ModelViewSet):
             rest_sec=Subquery(heaviest_set_subquery.values('rest_sec'), output_field=IntegerField())
         ).values('weight_kg', 'reps', 'rest_sec', exercise_id=F('pk'), exercise_name=F('name')).distinct()
 
-        # approach without calling subqueries multiple times, but the queries look even more inefficient
-        from django.db import connection, reset_queries
-        from django.db.models import Prefetch
-        reset_queries()
-        heaviest_sets_x = self.get_queryset().prefetch_related( 
-            Prefetch(
-                'exerciserealization_set__exerciseset_set',
-                queryset=ExerciseSet.objects.filter(
-                    exercise_realization__workout__user_id=request.user.user.id
-                ).order_by('-weight_kg', '-reps', 'rest_sec')[:1],
-                to_attr='heaviest_seat',
-            )
-        )
-        res = []
-        values = heaviest_sets_x.values(exercise_id=F('id'), exercise_name=F('name'))
-        for i, row in enumerate(values):
-            heaviest_set = heaviest_sets_x[i]._prefetched_objects_cache['exerciserealization_set'][0].heaviest_seat[0]
-            row['weight_kg'] = heaviest_set.weight_kg
-            row['reps'] = heaviest_set.reps
-            row['rest_sec'] = heaviest_set.rest_sec
-            res.append(row)
-        
-        print(res)
-        print(connection.queries)
-        # print(heaviest_setsa.values(x=F('heaviest_seat')))
-        # print(heaviest_sets.heaviest_sets)
-        return Response(connection.queries)
+        # 2. approach without calling subqueries multiple times, but the queries look even more inefficient
+        # heaviest_sets_x = self.get_queryset().prefetch_related( 
+        #     Prefetch(
+        #         'exerciserealization_set__exerciseset_set',
+        #         queryset=ExerciseSet.objects.filter(
+        #             exercise_realization__workout__user_id=request.user.user.id
+        #         ).order_by('-weight_kg', '-reps', 'rest_sec')[:1],
+        #         to_attr='heaviest_seat',
+        #     )
+        # )
+        # res = []
+        # values = heaviest_sets_x.values(exercise_id=F('id'), exercise_name=F('name'))
+        # for i, row in enumerate(values):
+        #     heaviest_set = heaviest_sets_x[i]._prefetched_objects_cache['exerciserealization_set']
+        #     if len(heaviest_set) > 0:
+        #         heaviest_set = heaviest_set[0].heaviest_seat
+        #         if len(heaviest_set) > 0:
+        #             heaviest_set = heaviest_set[0]
+        #             row['weight_kg'] = heaviest_set.weight_kg
+        #             row['reps'] = heaviest_set.reps
+        #             row['rest_sec'] = heaviest_set.rest_sec
+        #     res.append(row)
+
+        return Response(heaviest_sets)
 
         
-        # 2.approach using raw SQL postgres and postgres json functions (json forming could also be done in memory (dictfetchall) or via serializers)
+        # 3. approach using raw SQL postgres and postgres json functions (json forming could also be done in memory (dictfetchall) or via serializers)
         # rawqueryserializer: https://copyprogramming.com/howto/serializing-raw-sql-query-django
         # todo: compare speed of the approaches
         # raw_query = """SELECT (select row_to_json(_) from (select ex.id, ex.name, es.weight_kg, es.reps, es.rest_sec) as _)
